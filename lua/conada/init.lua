1ec08ef03287dfa281640aeeaf13c2fd5fcd785d
Connor Adams <cleeadacon909@gmail.com>
2024-08-25 12:31:23 -0700

    nvim setup init

---
+++ lua/conada/init.lua
@@ -0,0 +1,4 @@
+require("conada.remap")
+require("conada.packer")
+require("conada.set")
+print("Hello from Conada")
---
+++ lua/conada/remap.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+vim.keymap.set("n", "<C-d>pv", "<C-d>zz")
+vim.keymap.set("n", "<C-u>pv", "<C-u>zz")
+
